23e055bc4dbd414ec3af857eafe15264f1bd7f59
Gavin Fynbo <gavfynbo@gmail.com>
2018-01-02 21:42:52 -0500

    update directions class

--- src/Directions.java
+++ src/Directions.java
@@ -1,13 +1,43 @@
+import java.util.Scanner;
+
 public class Directions {
     String[] directions;
 
     public Directions(int numSteps) {
         System.out.println("This is the directions constructor!");
         directions = new String[numSteps];
+
+        Scanner getDirections = new Scanner(System.in);
+
+        // have the user input the directions
+        for (int i = 0; i < numSteps; i++) {
+            System.out.println("Input step " + (i + 1) + ": ");
+            directions[i] = getDirections.nextLine();
+        }
+        // return directions to user so they can see what they wrote
+        for (int i = 0; i < numSteps; i++) {
+            System.out.println("Step " + (i + 1) + ": " + directions[i]);
+        }
     }
 
     public Directions() {
-        System.out.println("Please input the number of steps.");
+        // get the number of steps
+        Scanner getDirections = new Scanner(System.in);
+        System.out.println("Input the total number of steps here: ");
+        int numSteps = Integer.parseInt(getDirections.nextLine());
+
+        // setup directions
+        directions = new String[numSteps];
+
+        // have the user input the directions
+        for (int i = 0; i < numSteps; i++) {
+            System.out.println("Input step " + (i + 1) + ": ");
+            directions[i] = getDirections.nextLine();
+        }
+        // return directions to user so they can see what they wrote
+        for (int i = 0; i < numSteps; i++) {
+            System.out.println("Step " + (i + 1) + ": " + directions[i]);
+        }
     }
 
     public int returnDirectionCount() {
--- src/RecipeManager.java
+++ src/RecipeManager.java
@@ -2,7 +2,7 @@ public class RecipeManager {
     public static void main(String[] args) {
         System.out.println("Hello! Welcome to the RecipeManager.");
 
-        Directions dirs = new Directions(4);
+        Directions dirs = new Directions();
         CookTime time = new CookTime(15);
         Review rv = new Review(4);
         Ingredient[] ing = new Ingredient[5];
