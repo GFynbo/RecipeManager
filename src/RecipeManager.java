eea8be9e92961d6c9180a8877b9658fd64696941
Gavin Fynbo <gavfynbo@gmail.com>
2018-01-05 21:22:03 -0800

    add swing and frame elements

--- src/RecipeManager.java
+++ src/RecipeManager.java
@@ -1,19 +1,64 @@
-import java.util.Scanner;
+import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import java.awt.*;
+import java.awt.event.*;
 
 public class RecipeManager {
-    public static void main(String[] args) {
-        System.out.println("Hello! Welcome to the RecipeManager.");
-        Scanner sc = new Scanner(System.in);
+    private JPanel mainPanel;
+    private JTabbedPane tabbedPane1;
+    private JPanel existingRecipePane;
+    private JPanel newRecipePane;
+    private JList existingRecipeList;
+    private JTextField newRecipeName;
+    private JTextField newRecipeCookTime;
+    private JTextArea newRecipeDirections;
+    private JTextArea newRecipeIngredients;
+    private JButton createNewButton;
+    private JButton createButton;
+    private JEditorPane existingRecipeDisplay;
 
-        Directions dirs = new Directions();
-        CookTime time = new CookTime();
-        Review rv = new Review();
-        Ingredient ing = new Ingredient();
-        System.out.println("Input recipe name: ");
-        String recipeName = sc.nextLine();
+    public RecipeManager() {
+
+        existingRecipeList.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                System.out.println("Selected a new recipe!");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+
+        // create initial test recipe
+        System.out.println("Hello! Welcome to the RecipeManager.");
+        Directions dirs = new Directions(3);
+        CookTime time = new CookTime(15);
+        Review rv = new Review(4);
+        Ingredient ing = new Ingredient("Salt", "2 tbsp");
+        String recipeName = "Meatballs";
 
         Recipe meatballs = new Recipe(recipeName, ing, dirs, time, rv);
 
+        // run the frame for the actual app
+        JFrame frame = new JFrame("RecipeManager");
+        frame.setContentPane(new RecipeManager().mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
         System.out.println(meatballs);
+
+    }
+
+    public void setData(Recipe data) {
+        
+    }
+
+    public void getData(Recipe data) {
+    }
+
+    public boolean isModified(Recipe data) {
+        return false;
     }
 }
