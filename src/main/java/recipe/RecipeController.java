5b125ff20bb745e6fc34f080ffd5be08225971da
Gavin Fynbo <gavfynbo@gmail.com>
2018-01-07 15:07:47 -0800

    add and fix .fxml and controller/main classes

--- src/main/java/recipe/RecipeController.java
+++ src/main/java/recipe/RecipeController.java
@@ -1,4 +1,13 @@
 package recipe;
 
+import javafx.scene.control.Label;
+
+import javafx.event.ActionEvent;
+
 public class RecipeController {
+    public Label helloWorld;
+
+    public void sayHelloWorld(ActionEvent actionEvent) {
+        this.helloWorld.setText("Hello, world!");
+    }
 }
