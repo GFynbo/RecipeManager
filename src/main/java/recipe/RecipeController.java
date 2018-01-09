4c1e1ba3959bdbcdb0c3e3e83fb8ff70cd1c706d
Gavin Fynbo <gavfynbo@gmail.com>
2018-01-09 14:05:41 -0800

    update recipemanager and controller

--- src/main/java/recipe/RecipeController.java
+++ src/main/java/recipe/RecipeController.java
@@ -14,18 +14,43 @@ import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.stage.Stage;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
 public class RecipeController {
 
-    @FXML private Parent root;
     @FXML private MenuBar myMenu;
-    @FXML private MenuItem myMenuClose;
+    @FXML private ListView<Recipe> recipeList;
 
     @FXML
     protected void closeWindow(ActionEvent actionEvent) {
         Stage stage = (Stage) myMenu.getScene().getWindow();
         stage.close();
+    }
+
+    @FXML
+    protected void addRecipe(ActionEvent actionEvent) {
+        ObservableList<Recipe> recipes = FXCollections.observableArrayList();
+
+        String name = "Meatballs";
+        Ingredient ings = new Ingredient("Salt", "1 Tbsp");
+        Directions direct = new Directions(3);
+        CookTime ct = new CookTime(15);
+        Review rv = new Review(4);
+
+        Recipe mb = new Recipe(name, ings, direct, ct, rv);
+        recipes.add(mb);
+
+        recipeList = new ListView<>(recipes);
+
+        recipeList.setCellFactory(param -> new ListCell<Recipe>() {
+            @Override
+            protected void updateItem(Recipe recp, boolean empty) {
+                super.updateItem(recp, empty);
+
+                if (empty || recp == null || recp.toString() == null) {
+                    setText(null);
+                } else {
+                    setText(recp.toString());
+                }
+            }
+        });
     }
 }
--- src/main/java/recipe/RecipeManager.java
+++ src/main/java/recipe/RecipeManager.java
@@ -15,32 +15,7 @@ public class RecipeManager extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("recipe.fxml"));
-        ObservableList<Recipe> recipes = FXCollections.observableArrayList();
 
-        String name = "Meatballs";
-        Ingredient ings = new Ingredient("Salt", "1 Tbsp");
-        Directions direct = new Directions(3);
-        CookTime ct = new CookTime(15);
-        Review rv = new Review(4);
-
-        Recipe mb = new Recipe(name, ings, direct, ct, rv);
-
-        recipes.add(mb);
-
-        ListView<Recipe> recipeList = new ListView<>(recipes);
-
-        recipeList.setCellFactory(param -> new ListCell<Recipe>() {
-            @Override
-            protected void updateItem(Recipe recp, boolean empty) {
-                super.updateItem(recp, empty);
-
-                if (empty || recp == null || recp.toString() == null) {
-                    setText(null);
-                } else {
-                    setText(recp.toString());
-                }
-            }
-        });
         primaryStage.setTitle("RecipeManager");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
