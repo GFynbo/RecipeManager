6e231a2afa3d865bb8d6592982aaf66c44927cf6
Gavin Fynbo <gavfynbo@gmail.com>
2018-01-09 13:57:47 -0800

    add close window button

--- src/main/java/recipe/RecipeController.java
+++ src/main/java/recipe/RecipeController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.event.ActionEvent;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
@@ -18,15 +19,13 @@ import java.util.ResourceBundle;
 
 public class RecipeController {
 
-    @FXML
-    private MenuBar myMenu;
-    private MenuItem myMenuClose;
+    @FXML private Parent root;
+    @FXML private MenuBar myMenu;
+    @FXML private MenuItem myMenuClose;
 
     @FXML
     protected void closeWindow(ActionEvent actionEvent) {
-        Stage stg = (Stage)myMenu.getScene().getWindow();
-        Scene scn = stg.getScene();
-        stg.setScene(scn);
-        stg.close();
+        Stage stage = (Stage) myMenu.getScene().getWindow();
+        stage.close();
     }
 }
